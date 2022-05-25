746f5f8b8886fcbaea92804dc148143563fe753f
brady444 <16694009+brady444@users.noreply.github.com>
2022-05-25 10:12:20 -0500

    changed about page to only show text before errors if there are errors, updated parser form text font size, updated vocabulary data

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,18 +17,21 @@ pages.about = {
 				<p class = "extraSmallFont gray">Mounce, William D.. Basics of Biblical Greek Grammar (Zondervan Language Basics Series). Zondervan Academic. (text might be modified)</p>
 			</div>
 			
-			<div class = "flexColumnTop halfWidth largeGap">
-				<p class = "smallFont">This website might use automated systems to retrieve and format data. These systems might output some potential inaccuracies or errors with the data. Some or all of the errors may be outdated or inaccurate. Some or all of the errors are listed below. Some of the errors might not be listed.</p>
-				
-				<div class = "flexColumnLeft smallGap">
-					${ constants.vocabularyErrors.scraperErrors.map (error => html
-						`<p class = "extraSmallFont">${ error }</p>`
-					) }
+			${ constants.vocabularyErrors.scraperErrors.length > 0 || constants.vocabularyErrors.errors.length > 0 ? html
+				`<div class = "flexColumnTop halfWidth largeGap">
+					<p class = "smallFont">This website might use automated systems to retrieve and format data. These systems might output some potential inaccuracies or errors with the data. Some or all of the errors may be outdated or inaccurate. Some or all of the errors are listed below. Some of the errors might not be listed.</p>
 					
-					${ constants.vocabularyErrors.errors.map (error => html
-						`<p class = "extraSmallFont">${ error }</p>`
-					) }
-				</div>
-			</div>
+					<div class = "flexColumnLeft smallGap">
+						${ constants.vocabularyErrors.scraperErrors.map (error => html
+							`<p class = "extraSmallFont">${ error }</p>`
+						) }
+						
+						${ constants.vocabularyErrors.errors.map (error => html
+							`<p class = "extraSmallFont">${ error }</p>`
+						) }
+					</div>
+				</div>` :
+				null
+			}
 		</div>`
 };
